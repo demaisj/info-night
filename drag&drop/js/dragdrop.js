69c7e9c6c1a082210e7ba902e887a08dd295a689
Freebios <quentin.galliano@epitech.eu>
2016-12-01 23:24:32 +0100

    Drag&drop tests

---
+++ drag&drop/js/dragdrop.js
@@ -0,0 +1,48 @@
+// target elements with the "draggable" class
+interact('.movable')
+.draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
+
+    // call this function on every dragmove event
+    onmove: dragMoveListener
+})
+.dropzone({
+	accept: '.menu',
+});
+
+function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+}
+
+// this is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
+
+function spawnModule()
+{
+	div = document.createElement("div");
+	div.setAttribute("class", "movable");
+	div.setAttribute("style", "z-index:1; height:50px; width:50px; position:absolute; background-color:blue;");
+	div.innerHTML = "GREG";
+	document.getElementById("page").appendChild(div);
+}
