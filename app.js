a40fe0d1bf3cdec465e92908281c5bf3e1c1a4d4
Gregoire Lodi <gregoire.lodi@epitech.eu>
2016-12-02 00:26:21 +0100

    if data corresponding to the page, sending data to template, else 404

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-var express = require('express')
-    expressLayouts = require('express-ejs-layouts');
+var express    = require("express"),
+    ejslayouts = require("express-ejs-layouts"),
+    mongo      = require("mongodb").MongoClient,
+    assert     = require("assert")
+    colors     = require("colors");
+
+const dbURL = "mongodb://localhost:27017/airbus";
 var app = express()
 
 app.set("view engine", "ejs");
-app.use(expressLayouts);
+app.use(ejslayouts);
 app.use(express.static("public"));
 
-app.get('/', function (req, res) {
-  res.render("body", { title: "test123"})
+app.use(function(req, res, next) {
+  if (!req.url.match(/css|js/gi))
+    console.log(`Page ${req.url.bold.green} requested at ${new Date().toLocaleTimeString().bold.blue}...`);
+  next();
+})
+
+mongo.connect(dbURL, function(error, db) {
+  assert.equal(null, error);
+  console.log(`MongoDB is listenting on ${dbURL}`);
+
+  app.get("/", function (req, res) {
+    res.render("index", { title: "Bienvenue !"})
+  })
+
+  app.get("/:page", function(req, res) {
+    let page = req.params.page;
+
+    res.render(page, { title: page }, function(error, html) {
+      if (error)
+        res.render("404", { title: "404 !" });
+      res.send(html);
+    });
+  })
+
+  app.get("/views/:page", function(req, res) {
+    let page = req.params.page;
+    let query = { "_id": page };
+
+    db.collection("pages").find(query).toArray(function(error, data) {
+      res.render(page, { title: page, data: data }, function(error, html) {
+        if (error)
+          res.render("404", { title: "404 !" });
+        res.send(html);
+      });
+    })
+  })
+
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log("Server listening on port 8080!")
 })
