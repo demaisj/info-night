fc172faa2488296e5cab331480152a21fe65ce4e
Gregoire Lodi <gregoire.lodi@epitech.eu>
2016-12-01 21:22:21 +0100

    mdl

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express()
 
 app.set("view engine", "ejs");
 app.use(expressLayouts);
+app.use(express.static("public"));
 
 app.get('/', function (req, res) {
   res.render("body", { title: "test123"})
